ae2395ac392c7d458b723c3b36a6dad3515a8382
Eduardo Ibarra <eibarra42@arizona.edu>
2023-12-02 13:31:33 -0700

    Add script that populates the database with required data.

---
+++ db/required_data.sql
@@ -0,0 +1,32 @@
+-- This script adds all of the required data into the database.
+
+insert into trainer values (1, 'Ash Ketchum', '(760)555-5555'); -- Need at least one trainer.
+
+-- junior courses
+insert into course values ('Strength 001', 1, 'monday', CURRENT_DATE, '11-2-2025', 1200, 1300, 0, 100);
+insert into course values ('Yoga 001', 1, 'tuesday', CURRENT_DATE, '11-2-2025', 1200, 1300, 0, 100);
+
+-- senior courses
+insert into course values ('Strength 002', 1, 'wednesday', CURRENT_DATE, '11-2-2025', 1200, 1300, 0, 100);
+insert into course values ('Yoga 002', 1, 'thursday', CURRENT_DATE, '11-2-2025', 1200, 1300, 0, 100);
+
+insert into package values ('Junior', 1);
+insert into package values ('Senior', 1);
+insert into package values ('Strength', 1);
+
+insert into packagecourse values ('Junior', 'Strength 001');
+insert into packagecourse values ('Junior', 'Yoga 001');
+
+insert into packagecourse values ('Senior', 'Strength 002');
+insert into packagecourse values ('Senior', 'Yoga 002');
+
+insert into packagecourse values ('Strength', 'Strength 001');
+insert into packagecourse values ('Strength', 'Strength 002');
+
+insert into levels values ('Gold', 20, 1000);
+insert into levels values ('Diamond', 10, 500);
+insert into levels values ('Regular', 0, 0);
+
+-- GYM Equipment
+insert into equipment values ('Barbell', 10, 10);
+insert into equipment values ('Yoga mat', 20, 20);
