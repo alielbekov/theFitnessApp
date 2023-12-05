900e7698195bae8ee53d0cc52015c7fa45be80b2
Ali Elbekov <eldorbek2001@arizona.edu>
2023-12-05 01:42:59 -0700

    add member partially ready, missing expenditure update

--- ali/Prog4.java
+++ ali/Prog4.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Prog4 {
     public static void main(String[] args) {
@@ -63,7 +65,7 @@ public class Prog4 {
             // Get user input from getNextAction()
             switch (choice) {
                 case 1:
-                    // Insert a new member into the database
+                    insertMember(dbconn);
                     break;
                 case 2:
                     // Delete a member from the database
@@ -94,6 +96,7 @@ public class Prog4 {
                 case 4:
                     // TODO:
                     break;
+                case 5: 
             }
 
             // Shut down the connection to the DBMS.
@@ -139,13 +142,222 @@ public class Prog4 {
     }
 
     // Add a new method for record insertion
-    private static void insertMember(Statement stmt) throws SQLException {
-        // Implement logic to get member information from the user and insert it into
-        // the database
-        // Update related records based on the selected course package
-        // Make sure to handle any exceptions that may occur during the insertion
-        // process
+private static void insertMember(Connection dbconn) throws SQLException {   
+    Scanner sc = new Scanner(System.in);
+
+    // Prompt for member information
+    System.out.print("Enter member name: ");
+    String memberName = sc.nextLine();
+
+    System.out.print("Enter member phone number: ");
+    String memberPhone = sc.nextLine();
+
+    // Fetch the highest existing member ID and increment it
+    int memberId = getNextMemberId(dbconn);
+
+    // Insert the member into the database
+    String insertSql = "INSERT INTO Member (id, name, phone, membershipLevel, totalSpending) VALUES (?, ?, ?, 'Regular', 0.0)";
+    PreparedStatement pstmt = dbconn.prepareStatement(insertSql);
+
+    pstmt.setInt(1, memberId);
+    pstmt.setString(2, memberName);
+    pstmt.setString(3, memberPhone);
+
+    int rowsAffected = pstmt.executeUpdate();
+    if (rowsAffected > 0) {
+        System.out.println("Member added successfully.");
+        printAllMembers(dbconn);
+        displayPackagesWithCourses(dbconn);
+        // Display available packages and handle package selection
+    } else {
+        System.out.println("Error: Member could not be added.");
     }
+
+    pstmt.close();
+}
+
+private static void displayPackagesWithCourses(Connection dbconn) throws SQLException {
+    String packageQuery = "SELECT name, price FROM Package ORDER BY name";
+    Statement packageStmt = dbconn.createStatement();
+    ResultSet packageRs = packageStmt.executeQuery(packageQuery);
+
+    List<String> packageNames = new ArrayList<>();
+    int index = 1;
+
+    while (packageRs.next()) {
+        String packageName = packageRs.getString("name");
+        double packagePrice = packageRs.getDouble("price");
+
+        if (areCoursesAvailable(dbconn, packageName)) {
+            packageNames.add(packageName); // Store package names for later reference
+            System.out.printf("%d: %s (Price: $%.2f)\n", index++, packageName, packagePrice);
+            displayCoursesForPackage(dbconn, packageName);
+        }
+    }
+
+    packageStmt.close();
+    selectAndLinkPackage(dbconn, packageNames);
+}
+private static boolean areCoursesAvailable(Connection dbconn, String packageName) throws SQLException {
+    String courseQuery = "SELECT c.maxParticipants, c.currentParticipants " +
+                         "FROM Course c JOIN PackageCourse pc ON c.name = pc.courseName " +
+                         "WHERE pc.packageName = ?";
+    PreparedStatement courseStmt = dbconn.prepareStatement(courseQuery);
+    courseStmt.setString(1, packageName);
+    ResultSet courseRs = courseStmt.executeQuery();
+
+    while (courseRs.next()) {
+        int maxParticipants = courseRs.getInt("maxParticipants");
+        int currentParticipants = courseRs.getInt("currentParticipants");
+        if (currentParticipants >= maxParticipants) {
+            courseStmt.close();
+            return false; // Course is full, do not show this package
+        }
+    }
+
+    courseStmt.close();
+    return true; // All courses in the package have available space
+}
+
+private static void displayCoursesForPackage(Connection dbconn, String packageName) throws SQLException {
+    String courseQuery = "SELECT c.name, c.currentParticipants, c.maxParticipants " +
+                         "FROM Course c JOIN PackageCourse pc ON c.name = pc.courseName " +
+                         "WHERE pc.packageName = ?";
+    PreparedStatement courseStmt = dbconn.prepareStatement(courseQuery);
+    courseStmt.setString(1, packageName);
+    ResultSet courseRs = courseStmt.executeQuery();
+
+    while (courseRs.next()) {
+        String courseName = courseRs.getString("name");
+        int currentParticipants = courseRs.getInt("currentParticipants");
+        int maxParticipants = courseRs.getInt("maxParticipants");
+        System.out.printf("    Course: %s (Available: %d/%d)\n", courseName, currentParticipants, maxParticipants);
+    }
+    courseStmt.close();
+}
+
+private static void printAllMembers(Connection dbconn) throws SQLException {
+    String query = "SELECT * FROM Member";
+    Statement stmt = dbconn.createStatement();
+    ResultSet rs = stmt.executeQuery(query);
+
+    System.out.println("ID\tName\t\tPhone\t\tMembership Level\tTotal Spending");
+    System.out.println("-------------------------------------------------------------------------");
+    while (rs.next()) {
+        int id = rs.getInt("id");
+        String name = rs.getString("name");
+        String phone = rs.getString("phone");
+        String membershipLevel = rs.getString("membershipLevel");
+        double totalSpending = rs.getDouble("totalSpending");
+
+        System.out.printf("%d\t%s\t%s\t%s\t\t%.2f\n", id, name, phone, membershipLevel, totalSpending);
+    }
+
+    stmt.close();
+}
+
+private static void selectAndLinkPackage(Connection dbconn, List<String> packageNames) throws SQLException {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Select a package number: ");
+    int choice = sc.nextInt();
+
+    if (choice < 1 || choice > packageNames.size()) {
+        System.out.println("Invalid choice. Please try again.");
+        return;
+    }
+
+    // Fetching the memberId of the most recently added member
+    // Assuming you are calling this right after adding a member
+    int memberId = getLastInsertedMemberId(dbconn);
+
+    String selectedPackageName = packageNames.get(choice - 1);
+    linkMemberToPackage(dbconn, memberId, selectedPackageName);
+}
+
+
+
+private static void linkMemberToPackage(Connection dbconn, int memberId, String packageName) throws SQLException {
+    String insertSql = "INSERT INTO PackageMembers (packageName, memberId) VALUES (?, ?)";
+    PreparedStatement pstmt = dbconn.prepareStatement(insertSql);
+
+    pstmt.setString(1, packageName);
+    pstmt.setInt(2, memberId);
+
+    int rowsAffected = pstmt.executeUpdate();
+    if (rowsAffected > 0) {
+        System.out.println("Member successfully linked to package " + packageName + ".");
+        printPackageMembers(dbconn);
+        updateCourseParticipants(dbconn, packageName);
+
+
+    } else {
+        System.out.println("Error: Could not link member to package.");
+    }
+
+    pstmt.close();
+}
+private static void updateCourseParticipants(Connection dbconn, String packageName) throws SQLException {
+    String courseQuery = "SELECT courseName FROM PackageCourse WHERE packageName = ?";
+    PreparedStatement courseStmt = dbconn.prepareStatement(courseQuery);
+    courseStmt.setString(1, packageName);
+    ResultSet courseRs = courseStmt.executeQuery();
+
+    while (courseRs.next()) {
+        String courseName = courseRs.getString("courseName");
+        String updateSql = "UPDATE Course SET currentParticipants = currentParticipants + 1 WHERE name = ?";
+        PreparedStatement updateStmt = dbconn.prepareStatement(updateSql);
+        updateStmt.setString(1, courseName);
+        updateStmt.executeUpdate();
+        updateStmt.close();
+    }
+    courseStmt.close();
+}
+
+private static void printPackageMembers(Connection dbconn) throws SQLException {
+    String query = "SELECT * FROM PackageMembers";
+    Statement stmt = dbconn.createStatement();
+    ResultSet rs = stmt.executeQuery(query);
+
+    System.out.println("Package Name\tMember ID");
+    System.out.println("-------------------------");
+    while (rs.next()) {
+        String packageName = rs.getString("packageName");
+        int memberId = rs.getInt("memberId");
+        System.out.printf("%s\t\t%d\n", packageName, memberId);
+    }
+
+    stmt.close();
+}
+
+
+private static int getNextMemberId(Connection dbconn) throws SQLException {
+    String query = "SELECT MAX(id) FROM Member";
+    Statement stmt = dbconn.createStatement();
+    ResultSet rs = stmt.executeQuery(query);
+
+    int nextId = 1; // Start from 1 if no members exist
+    if (rs.next()) {
+        nextId = rs.getInt(1) + 1; // Increment the highest ID by 1
+    }
+
+    stmt.close();
+    return nextId;
+}
+
+private static int getLastInsertedMemberId(Connection dbconn) throws SQLException {
+    String query = "SELECT MAX(id) FROM Member";
+    Statement stmt = dbconn.createStatement();
+    ResultSet rs = stmt.executeQuery(query);
+
+    if (rs.next()) {
+        return rs.getInt(1);
+    }
+
+    stmt.close();
+    return -1; // Or handle this scenario appropriately
+}
+
+
 
     // Add a new method for record deletion
     private static void deleteMember(Statement stmt) throws SQLException {
